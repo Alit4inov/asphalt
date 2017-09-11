3d9ab2fb523687bb26eaca3e28eabbf9126bb05e
Alit4inov <alit4inov@gmail.com>
2017-09-11 08:34:40 +0300

    pre finish

--- src/js/app.js
+++ src/js/app.js
@@ -4,43 +4,48 @@ import HP from './modules/helpers';
 
 // When DOM is ready
 $(function () {
-HP.random(10, 20);
-});
-
-$(function() {
-        $(".nav-opener").click(function() {
-            $("body").toggleClass("nav-open");
-        });
-    });
-
-$(document).ready(function() {
-        $('.owl-carousel').owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: false,
-            dots: false,
-            nav: true
-        });
-    });
-
-$(document).ready(function() {
-        $('.grayscale').gray();
-    });
 
 
-$(document).ready(function(){
+    HP.random(10, 20);
+
+    $(".nav-opener").click(function() {
+                $("body").toggleClass("nav-open");
+            });
+
+
+    $('.owl-carousel').owlCarousel({
+                items: 1,
+                loop: true,
+                autoplay: false,
+                dots: false,
+                nav: true
+            });
+
+    $('.grayscale').gray();
+
+
     $(".thumb").hover(function(){
-        $("img", this).toggleClass("grayscale");
+            $("img", this).toggleClass("grayscale");
+        });
+
+    $().fancybox({
+      selector : '[data-fancybox="images"]',
+      loop     : true
     });
+
 });
 
 
 
 
-$().fancybox({
-  selector : '[data-fancybox="images"]',
-  loop     : true
-});
+
+
+
+
+
+
+
+
 
 
 
